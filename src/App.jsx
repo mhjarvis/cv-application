a8f0582d84e872ef36f1ea9beaededc6e5e36f09
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-02-21 18:37:12 -0600

    Add PersonalInformation component to layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import PersonalInformation from './components/PersonalInformation'
 
 function App() {
 
   return (
     <div className='app-container'>
       <div className='input-section'>
-
+        <PersonalInformation />
       </div>
       <div className='resume-section'>
 
