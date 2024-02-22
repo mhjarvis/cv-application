a6fcbc73878c763b92b3c221acd2b42ba32a4d2e
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-02-21 21:24:16 -0600

    Update component using props

--- src/components/InputSpan.jsx
+++ src/components/InputSpan.jsx
@@ -1,14 +1,26 @@
 /* eslint-disable react/prop-types */
 
+import { useState } from "react";
+
 export default function InputSpan({
   inputLabel,
   inputClass,
   inputPlaceHolder,
 }) {
+  const [userInput, setUserInput] = useState(inputPlaceHolder);
+
+  function handleInput(e) {
+    setUserInput(e);
+  }
+
   return (
     <span>
       <label>{inputLabel}</label>
-      <input className={inputClass} placeholder={inputPlaceHolder}></input>
+      <input
+        className={inputClass}
+        placeholder={userInput}
+        onChange={(event) => handleInput(event.target.value)}
+      ></input>
     </span>
   );
 }
