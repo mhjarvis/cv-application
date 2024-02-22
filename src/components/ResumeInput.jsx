e1b4ef4cfaf55291c46f69721d9109f9c1ab09fd
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-02-22 17:18:55 -0600

    Fix import errors

--- src/components/ResumeInput.jsx
+++ src/components/ResumeInput.jsx
@@ -1,9 +1,15 @@
+import PersonalDetails from './PersonalDetails'
+import Education from './Education'
+import Skills from './Skills'
+import ProfessionalExperience from './ProfessionalExperience'
 
 export default function ResumeInput() {
-
 	return (
-		<div className="container">Resume Input
-
+		<div className="container">
+			<PersonalDetails />
+			<Education />
+			<Skills />
+			<ProfessionalExperience />
 		</div>
 	)
 }
