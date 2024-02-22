9704bef3814362929965ef617a3723e404d1198c
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-02-22 13:47:04 -0600

    Reset components; Add css reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import { useState } from "react";
 import "./App.css";
-import PersonalInformation from "./components/PersonalInformation";
-import Resume from "./components/Resume";
+
 
 function App() {
-  const [personalInfo, setPersonalInfo] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    street: "",
-    city: "",
-    state: "",
-    zip: "",
-  });
 
   return (
-    <div className="app-container">
-      <div className="input-section">
-        <PersonalInformation />
-      </div>
-      <div className="resume-section">
-        <Resume pinfo={personalInfo} />
-      </div>
+    <div className="container">
+      
     </div>
   );
 }
