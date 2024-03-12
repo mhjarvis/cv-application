e7c318852e8b6f96db6b723603027001d5fc3726
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-12 11:20:50 -0500

    Minor updates to css colors and layout; Add additional input fields to Personal Details component

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -2,14 +2,34 @@ import "../styles/PersonalDetails.css";
 import InputElement from "./InputElement";
 
 export default function PersonalDetails() {
-  return (
-    <div className="personal-details-container">
-      <h2 className="personal-details-header">Personal Details</h2>
-      <InputElement
-        inputLabel="FULL NAME"
-        inputClass="full-name"
-        inputPlaceHolder="Enter your full name"
-      />
-    </div>
-  );
+    return (
+        <div className="personal-details-container">
+            <h2 className="personal-details-header">Personal Details</h2>
+            <InputElement
+                inputLabel="Full Name"
+                inputClass="full-name"
+                inputPlaceHolder="Enter your full name"
+            />
+            <InputElement
+                inputLabel="Email"
+                inputClass="email"
+                inputPlaceHolder="Enter your email"
+            />
+            <InputElement
+                inputLabel="Phone Number"
+                inputClass="phone-number"
+                inputPlaceHolder="Enter your phone number"
+            />
+            <InputElement
+                inputLabel="LinkedIn"
+                inputClass="linkedin"
+                inputPlaceHolder="Enter LinkedIn"
+            />
+            <InputElement
+                inputLabel="Github"
+                inputClass="github"
+                inputPlaceHolder="Enter Github"
+            />
+        </div>
+    );
 }
