847a840045db5ea5ab51dc28599cf2c53cd8fdaf
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-13 22:57:16 -0500

    Move PersonalDetails to own component

---
+++ src/components/PersonalDetails.jsx
@@ -0,0 +1,30 @@
+export default function PersonalDetails() {
+    return (
+        <div className="personal-details">
+            <h2 className="personal-details-heading">Personal Details</h2>
+
+            <input type="text" className="fullname" placeholder="Full Name" />
+
+            <input type="text" className="fullname" placeholder="Full Name" />
+
+            <div className="contact-container">
+                <input
+                    type="text"
+                    className="fullname"
+                    placeholder="Full Name"
+                />
+                <input
+                    type="text"
+                    className="fullname"
+                    placeholder="Full Name"
+                />
+            </div>
+
+            <input type="text" className="fullname" placeholder="Full Name" />
+
+            <input type="text" className="fullname" placeholder="Full Name" />
+
+            <input type="text" className="fullname" placeholder="Full Name" />
+        </div>
+    );
+}
--- src/components/ResumeInput.jsx
+++ src/components/ResumeInput.jsx
@@ -1,52 +1,11 @@
+import Education from "./Education";
+import PersonalDetails from "./PersonalDetails";
+
 export default function ResumeInput() {
     return (
         <div>
-            <div className="personal-details">
-                <h2 className="personal-details-heading">Personal Details</h2>
-
-                <input
-                    type="text"
-                    className="fullname"
-                    placeholder="Full Name"
-                />
-
-                <input
-                    type="text"
-                    className="fullname"
-                    placeholder="Full Name"
-                />
-
-                <div className="contact-container">
-                    <input
-                        type="text"
-                        className="fullname"
-                        placeholder="Full Name"
-                    />
-                    <input
-                        type="text"
-                        className="fullname"
-                        placeholder="Full Name"
-                    />
-                </div>
-
-                <input
-                    type="text"
-                    className="fullname"
-                    placeholder="Full Name"
-                />
-
-                <input
-                    type="text"
-                    className="fullname"
-                    placeholder="Full Name"
-                />
-
-                <input
-                    type="text"
-                    className="fullname"
-                    placeholder="Full Name"
-                />
-            </div>
+            <PersonalDetails />
+            <Education />
         </div>
     );
 }
