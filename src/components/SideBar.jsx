5afb49e1d7745deb8f1fde17749ffd118f99b409
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-13 23:34:03 -0500

    Fix styling for sidebar; switch to grid and fixed sizes

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,9 +3,9 @@ import "../styles/SideBar.css"
 export default function SideBar() {
     return (
         <div className="sidebar">
-            <button className="btn content-btn">Content</button>
-            <button className="btn customize-btn">Customize</button>
-            <button className="btn download-btn">Download</button>
+            <button className="sidebar-btn content-btn">Content</button>
+            <button className="sidebar-btn customize-btn">Customize</button>
+            <button className="sidebar-btn download-btn">Download</button>
         </div>
     );
 }
