160fd298482b4771da52a7fba84e7d3739cb9b9a
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-14 10:43:16 -0500

    Reorganize buttons; add classnames, clipart images

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,11 +1,28 @@
-import "../styles/SideBar.css"
+import "../styles/SideBar.css";
+// import clip art images directly
+import noteImage from "../../public/images/note-edit-outline.svg";
+import pencilImage from "../../public/images/pencil-ruler-outline.svg";
+import downloadImage from "../../public/images/download-circle-outline.svg";
 
 export default function SideBar() {
     return (
         <div className="sidebar">
-            <button className="sidebar-btn content-btn">Content</button>
-            <button className="sidebar-btn customize-btn">Customize</button>
-            <button className="sidebar-btn download-btn">Download</button>
+            <button className="sidebar-btn content-btn">
+                <img src={noteImage} className="icon" alt="paper-clip-art" />
+                <p className="content-btn-p">Content</p>
+            </button>
+            <button className="sidebar-btn customize-btn">
+                <img src={pencilImage} className="icon" alt="pencil-clip-art" />
+                <p className="content-btn-p">Download</p>
+            </button>
+            <button className="sidebar-btn download-btn">
+                <img
+                    src={downloadImage}
+                    className="icon"
+                    alt="download-clip-art"
+                />
+                <p className="content-btn-p">Download</p>
+            </button>
         </div>
     );
 }
