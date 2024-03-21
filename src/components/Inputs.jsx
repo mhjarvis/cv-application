22a2dd697deb00c20c72ee38b7b991b7814e1106
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-21 12:37:19 -0500

    Create Input component default

---
+++ src/components/Inputs.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+
+export default function Imports(props) {
+    return (
+        <input
+            type={props.type}
+            className={props.className}
+            placeholder={props.placeholder}
+        />
+    );
+}
