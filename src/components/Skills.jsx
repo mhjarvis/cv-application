079aeb925d455a60aaae4dca20467612c73f0e61
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-03-21 12:37:07 -0500

    Import Inputs and add to flow

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,11 @@
+import Inputs from "./Inputs";
+
 export default function Skills() {
     return (
         <div className="skills-container section-container">
             <h2 className="section-heading skills-heading">Skills</h2>
             <label htmlFor="skill-input">Skill</label>
-            <input type="text" placeholder="Enter skill" />
+            <Inputs type="text" placeholder="Enter skill" />
             <button className="skill-btn">Add Skill</button>
         </div>
     );
