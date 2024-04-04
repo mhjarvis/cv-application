362f12f93f93e2029071cf4face7e5ec226232ff
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-04 08:46:17 -0500

    Add ButtonPanel component with Buttons and miminal styling

---
+++ src/components/ButtonPanel/ButtonPanel.jsx
@@ -0,0 +1,8 @@
+import "./ButtonPanel.css";
+
+export default function ButtonPanel() {
+    return <div className="button-panel">
+        <button>Content</button>
+        <button>Customize</button>
+    </div>;
+}
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,10 +1,11 @@
 import "./Resume.css";
+import ButtonPanel from "./ButtonPanel/ButtonPanel";
 
 export default function Resume() {
     return (
         <div className="main">
             <div className="input">
-                {" "}
+                <ButtonPanel />
                 BUTTON SECTION PERSONAL DETAILS EDUCATION DETAILS EXPERIENCE
                 DETAILS SKILLS CERTIFICATES
             </div>
