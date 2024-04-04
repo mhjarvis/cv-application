642d5d5a481549c3ae47b4c11612beb6493d3387
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-04 10:05:42 -0500

    Add button elemeents

--- src/components/ButtonPanel/ButtonPanel.jsx
+++ src/components/ButtonPanel/ButtonPanel.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
 import "./ButtonPanel.css";
 
 export default function ButtonPanel() {
-    return <div className="button-panel">
-        <button>Content</button>
-        <button>Customize</button>
-    </div>;
+    const [tabSelected, setTabSelected] = useState("content");
+
+    function tabSelectHandler(event) {
+        setTabSelected(event.target.value);
+    }
+
+    return (
+        <div className="button-panel">
+            <button
+                className={tabSelected === "content" ? "active" : 'white'}
+                value="content"
+                onClick={tabSelectHandler}
+            >
+                Content
+            </button>
+            <button
+                className={tabSelected === "customize" ? "active" : 'white'}
+                value="customize"
+                onClick={tabSelectHandler}
+            >
+                Customize
+            </button>
+        </div>
+    );
 }
