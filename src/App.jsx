3bb7fcaea0d8b2c4b8fc977d8df089f803d0921e
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-04 08:32:29 -0500

    Add css and general layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-
+import Resume from "./components/Resume";
 
 function App() {
     return (
         <div className="main">
-            <p>hello</p>
+            <Resume />
         </div>
     );
 }
---
+++ src/components/Resume.jsx
@@ -0,0 +1,14 @@
+import "./Resume.css";
+
+export default function Resume() {
+    return (
+        <div className="main">
+            <div className="input">
+                {" "}
+                BUTTON SECTION PERSONAL DETAILS EDUCATION DETAILS EXPERIENCE
+                DETAILS SKILLS CERTIFICATES
+            </div>
+            <div className="output">Output</div>
+        </div>
+    );
+}
