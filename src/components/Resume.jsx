12a5119f9bbf17ca4b426e3fc3636a4bc1621253
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-04 11:59:20 -0500

    Add personal info button

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 export default function Resume() {
     const [tabSelected, setTabSelected] = useState("content");
+    const [inputSelected, setInputSelected] = useState("personalInfo");
 
     function tabSelectHandler(event) {
         setTabSelected(event.target.value);
@@ -14,8 +15,12 @@ export default function Resume() {
         <div className="main">
             <div className="input">
                 <ButtonPanel tab={tabSelected} onTabUpdate={tabSelectHandler} />
-                BUTTON SECTION PERSONAL DETAILS EDUCATION DETAILS EXPERIENCE
-                DETAILS SKILLS CERTIFICATES
+
+                {inputSelected === "personalInfo" ? (
+                    <button>Personal Info</button>
+                ) : (
+                    ""
+                )}
             </div>
             <Output />
         </div>
