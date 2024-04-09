41f408f0f81a9535625b62ced4fc36063c5ed872
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-09 15:43:42 -0500

    Import all values managed by Resume.jsx and implement (working)

---
+++ src/components/PersonalInformationInput/PersonalInformationInput.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+
+export default function PersonalInformationInput({
+    fullName,
+    email,
+    onPersonalUpdate,
+}) {
+    /*     function fullNameHandler(event) {
+        setFullName(event.target.value);
+    }
+
+    function emailHandler(event) {
+        setEmail(event.target.value);
+    } */
+
+    return (
+        <div>
+            <form action="">
+                <label htmlFor="fullName">Full Name</label>
+                <input
+                    type="text"
+                    id="fullName"
+                    value={fullName}
+                    onChange={onPersonalUpdate}
+                />
+                <label htmlFor="email">Email</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={onPersonalUpdate}
+                />
+            </form>
+        </div>
+    );
+}
