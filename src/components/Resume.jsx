31d9ce458fb6f308dab09e74bc2007f635445690
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-09 15:43:23 -0500

    Add state and update functions for PersonalInfo components

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,6 +2,7 @@ import "./Resume.css";
 import ButtonPanel from "./ButtonPanel/ButtonPanel";
 import Output from "./Output/Output";
 import { useState } from "react";
+import PersonalInformationInput from "./PersonalInformationInput/PersonalInformationInput";
 
 export default function Resume() {
     const [tabSelected, setTabSelected] = useState("content");
@@ -10,12 +11,30 @@ export default function Resume() {
         setTabSelected(event.target.value);
     }
 
+    /* Personal Information State and Update Function */
+    const [personal, setPersonal] = useState({
+        fullName: "",
+        email: "",
+    });
+
+    function personalHandler(event) {
+        setPersonal({
+            ...personal, // copy the old fields
+            [event.target.id]: event.target.value,
+        });
+    }
+
     return (
         <div className="main">
             <div className="input">
                 <ButtonPanel tab={tabSelected} onTabUpdate={tabSelectHandler} />
                 <section className="input-section">
-                    
+                    <PersonalInformationInput
+                        className="personal-information-container"
+                        onPersonalUpdate={personalHandler}
+                        fullName={personal.fullName}
+                        email={personal.email}
+                    />
                 </section>
             </div>
             <Output />
