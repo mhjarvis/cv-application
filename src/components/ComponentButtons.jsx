134a9f0da103a2da7018ea401c7cebc17515465b
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-10 23:20:42 -0500

    Rework buttons; add type and correct onClick event

--- src/components/ComponentButtons.jsx
+++ src/components/ComponentButtons.jsx
@@ -1,10 +1,19 @@
+/* eslint-disable react/prop-types */
 import "./ComponentButtons.css";
 
-export default function ComponentButtons() {
+export default function ComponentButtons({ clearEducation }) {
     return (
         <div className="component-buttons-container">
-            <button className="component-buttons-save">Save</button>
-            <button className="component-buttons-clear">Clear</button>
+            <button className="component-buttons-save" type="button">
+                Save
+            </button>
+            <button
+                className="component-buttons-clear"
+                type="button"
+                onClick={clearEducation}
+            >
+                Clear
+            </button>
         </div>
     );
 }
