d634641de149dbf77e6b5c6a96749c47a7430206
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-11 09:47:04 -0500

    Conditionallly output Education elements

--- src/components/Education/EducationOutput.jsx
+++ src/components/Education/EducationOutput.jsx
@@ -9,11 +9,21 @@ export default function EducationOutput({ education }) {
             <Seperator />
             <div className="education-output-section">
                 <span className="education-output-left">
-                    {education.degree}, {education.school}
+                    {/* remove comma if additional information is not present */}
+                    {education.degree}
+                    {education.degree && education.school ? ", " : ""}
+                    {education.school}
                 </span>
                 <span className="education-output-right">
-                    {education.schoolStartDate} - {education.schoolEndDate} |{" "}
-                    {education.schoolCity}, {education.schoolState}
+                    {education.schoolStartDate}
+                    {education.schoolStartDate && education.schoolEndDate
+                        ? "-"
+                        : ""}
+                    {education.schoolEndDate}
+                    {education.schoolCity ? " | " : ""}
+                    {education.schoolCity}
+                    {education.schoolCity && education.schoolState ? ", " : ""}
+                    {education.schoolState}
                 </span>
             </div>
         </div>
