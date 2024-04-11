ecf1fb609a3fbdd401f311c1f5bd06e365d8ae93
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-10 22:13:23 -0500

    Add bold/italic base classes

--- src/components/ProfessionalExperience/ProfessionalExperienceOutput.jsx
+++ src/components/ProfessionalExperience/ProfessionalExperienceOutput.jsx
@@ -4,15 +4,15 @@ import Seperator from "../Output/Seperator";
 export default function ProfessionalExpierenceOutput({ professional }) {
     return (
         <div className="professional-output-component">
-            <h1>Professional Experience</h1>
+            <h1 className="bold">Professional Experience</h1>
             <Seperator />
             <div className="professional-output-section">
                 <div className="professional-output-top">
                     {" "}
-                    <span className="professional-output-top-left">
-                        {" "}
-                        {professional.employer}, {professional.jobTitle}
-                    </span>
+                    <p className="professional-output-top-left">
+                        <span className="bold">{professional.employer}</span>,{" "}
+                        <span className="italic">{professional.jobTitle}</span>
+                    </p>
                     <span className="professional-output-top-right">
                         {professional.employmentStartDate} -{" "}
                         {professional.employmentEndDate}
