dcf5762cccf9159cbd9bfcef7c996ea69a68b397
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-10 22:32:29 -0500

    Create ComponentButtons component that handle save/clear actions

---
+++ src/components/ComponentButtons.jsx
@@ -0,0 +1,10 @@
+import "./ComponentButtons.css";
+
+export default function ComponentButtons() {
+    return (
+        <div className="component-buttons-container">
+            <button className="component-buttons-save">Save</button>
+            <button className="component-buttons-clear">Clear</button>
+        </div>
+    );
+}
