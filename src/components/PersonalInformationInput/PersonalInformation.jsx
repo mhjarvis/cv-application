b6443811857286af244d4b1be6a66aa229648975
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-11 08:40:58 -0500

    Naming update; Add files to hold PersonalInformation output

---
+++ src/components/PersonalInformationInput/PersonalInformation.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable react/prop-types */
+import "./PersonalInformation.css";
+import LabelWithInput from "../LabelWithInput";
+import ComponentButtons from "../ComponentButtons";
+
+export default function PersonalInformationInput({
+    onPersonalUpdate,
+    formClasses,
+    personal,
+    clearPersonal,
+}) {
+    return (
+        <div>
+            <form action="" className={formClasses}>
+                <h1 className="personal-info-title">Personal Information</h1>
+                <LabelWithInput
+                    id="fullName"
+                    labelTitle="FullName"
+                    inputType="text"
+                    value={personal.fullName}
+                    onChangeHandler={onPersonalUpdate}
+                />
+                <LabelWithInput
+                    id="email"
+                    labelTitle="Email"
+                    inputType="text"
+                    value={personal.email}
+                    onChangeHandler={onPersonalUpdate}
+                />
+                <LabelWithInput
+                    id="phoneNumber"
+                    labelTitle="Phone Number"
+                    inputType="text"
+                    value={personal.phoneNumber}
+                    onChangeHandler={onPersonalUpdate}
+                />{" "}
+                <LabelWithInput
+                    id="address"
+                    labelTitle="Address"
+                    inputType="text"
+                    value={personal.address}
+                    onChangeHandler={onPersonalUpdate}
+                />
+                <LabelWithInput
+                    id="linkedIn"
+                    labelTitle="linkedIn"
+                    inputType="text"
+                    value={personal.linkedIn}
+                    onChangeHandler={onPersonalUpdate}
+                />{" "}
+                <LabelWithInput
+                    id="github"
+                    labelTitle="GitHub"
+                    inputType="text"
+                    value={personal.github}
+                    onChangeHandler={onPersonalUpdate}
+                />
+                <ComponentButtons clearComponent={clearPersonal} />
+            </form>
+        </div>
+    );
+}
