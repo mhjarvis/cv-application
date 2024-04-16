9a9313ea220a931807999c4762689bdc247dc02b
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-16 18:44:03 -0500

    remove unused log output

--- src/components/SavedElements.jsx
+++ src/components/SavedElements.jsx
@@ -3,14 +3,13 @@
 import "./SavedElements.css";
 
 export default function SavedElement({ arr, deleteEducationElement }) {
-    console.log(typeof WORKINGONTHISONE);
     return (
         <div className="saved-elements-container">
             <div className="saved-element">
                 {arr.map((el, index) => (
                     <>
                         <p key={index}>{el.degree}</p>
-                        <button onClick={deleteEducationElement}>x</button>
+                        <button key={el + index} onClick={deleteEducationElement}>x</button>
                     </>
                 ))}
             </div>
