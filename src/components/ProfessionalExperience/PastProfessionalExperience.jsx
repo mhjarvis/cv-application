8f22b44b3dd4818dd29ddaeaecbdbfb2514aa423
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-18 16:05:38 -0500

    import PastProf... css file and add filler content

--- src/components/ProfessionalExperience/PastProfessionalExperience.jsx
+++ src/components/ProfessionalExperience/PastProfessionalExperience.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
+
+import "./ProfessionalExperience.css";
 export default function PastProfessionalExperience({
     pastProfessional,
     deleteProfessionalExperience,
