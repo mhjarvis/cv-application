b46eedbf615b0449c4ca177a4eaaaa2c59e10dd5
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-18 14:10:19 -0500

    Add comments and reorganize functions

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -9,21 +9,30 @@ import Education from "./Education/Education";
 import ProfessionalExpierence from "./ProfessionalExperience/ProfessionalExperience";
 
 export default function Resume() {
+    /**
+     * Tab Selectors for switching between input (Content) and Customize sections
+     */
     const [tabSelected, setTabSelected] = useState("content");
+    function tabSelectHandler(event) {
+        setTabSelected(event.target.value);
+    }
+    /**
+     *  Input section state variables
+     */
     const [education, setEducation] = useState({
-        degree: "Psychology",
-        school: "University of Alabama",
-        schoolCity: "Birmingham",
-        schoolState: "Alabama",
-        schoolStartDate: "Sep 2017",
-        schoolEndDate: "May 2021",
+        degree: "",
+        school: "",
+        schoolCity: "",
+        schoolState: "",
+        schoolStartDate: "",
+        schoolEndDate: "",
     });
     const [personal, setPersonal] = useState({
-        fullName: "Markus Jarvis",
-        email: "markus.h.jarvis@gmail.com",
-        address: "123 Main Ave SE, Huntsville, AL 36362",
+        fullName: "Peter Williamson",
+        email: "petersawesomeemail@gmail.com",
+        address: "123 Main Ave SE, Madison, AL 34872",
         phoneNumber: "855.545.3421",
-        linkedIn: "linkedin.com/markusjarvis",
+        linkedIn: "linkedin.com/petertheprogrammer",
         github: "github.com/mhjarvis",
     });
     const [professional, setProfessional] = useState({
@@ -35,89 +44,85 @@ export default function Resume() {
         description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     });
-
-    /*     these states handle initial filler content / saved array states */
+    /**
+     * Stored content (filler / saved information by user)
+     */
     const [pastEducationArr, setPastEducationArr] = useState([
         {
-            degree: "English",
+            degree: "Life Sciences, B.S.",
             school: "University of Washington",
             schoolCity: "Seattle",
-            schoolState: "Washington",
+            schoolState: "WA",
             schoolStartDate: "Sep 2011",
             schoolEndDate: "May 2015",
         },
         {
-            degree: "Mathematics",
-            school: "University of Washington",
-            schoolCity: "Seattle",
-            schoolState: "Washington",
-            schoolStartDate: "Sep 2011",
-            schoolEndDate: "May 2015",
+            degree: "Software Development / CyberSecurity, B.S.",
+            school: "University of Portland",
+            schoolCity: "Portland",
+            schoolState: "OR",
+            schoolStartDate: "Sep 2015",
+            schoolEndDate: "May 2019",
         },
     ]);
     const [pastPersonal, setPastPersonal] = useState({
-        fullName: "Markus Jarvis",
-        email: "markus.h.jarvis@gmail.com",
-        address: "123 Main Ave SE, Huntsville, AL 36362",
+        fullName: "Peter Williamson",
+        email: "petersawesomeemail@gmail.com",
+        address: "123 Main Ave SE, Madison, AL 34872",
         phoneNumber: "855.545.3421",
-        linkedIn: "linkedin.com/markusjarvis",
+        linkedIn: "linkedin.com/petertheprogrammer",
         github: "github.com/mhjarvis",
     });
-
-    function tabSelectHandler(event) {
-        setTabSelected(event.target.value);
-    }
-
+    /**
+     * Input update functions
+     */
     function personalHandler(event) {
         setPersonal({
             ...personal, // copy the old fields
             [event.target.id]: event.target.value,
         });
     }
-
     function educationHandler(event) {
         setEducation({
             ...education, // copy the old fields
             [event.target.id]: event.target.value,
         });
     }
-
     function professionalHandler(event) {
         setProfessional({
             ...professional, // copy the old fields
             [event.target.id]: event.target.value,
         });
     }
-
+    /*
+     * Functions to handle saved information arrays/objects
+     */
     function addToEducationArray() {
         // Add current education state to arr
         setPastEducationArr([...pastEducationArr, education]);
         clearEducation();
     }
-
     function deleteEducationElement(index) {
         const arr = [...pastEducationArr];
         arr.splice(index, 1);
         setPastEducationArr(arr);
     }
-
     function editElementFromPastEducationArray(index) {
         setEducation({ ...pastEducationArr[index] });
         deleteEducationElement(index);
     }
-
     function addToPastPersonal() {
         console.log("saving yuppers");
     }
-
     function deletePastPersonal() {
         console.log("delete yuppers");
     }
-
     function editPastPersonal() {
         console.log("edititing yuppers");
     }
-
+    /*
+     * Functions to clear input fields / reset fields
+     */
     function clearEducation() {
         setEducation({
             degree: "",
