656b3979d9fa532bd2a3223d076e03c68c857213
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-19 13:49:44 -0500

    Map elements

--- src/components/ProfessionalExperience/PastProfessionalExperience.jsx
+++ src/components/ProfessionalExperience/PastProfessionalExperience.jsx
@@ -1,11 +1,50 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
+import icon_edit from "../../images/icon-edit.svg";
 
-import "./ProfessionalExperience.css";
+import "./PastProfessionalExperience.css";
 export default function PastProfessionalExperience({
     pastProfessional,
     deleteProfessionalExperience,
     editProfessionalExperience,
 }) {
-    return <div>stuff</div>;
+    if (pastProfessional.length > 0) {
+        return (
+            <div className="ppe-container">
+                {pastProfessional.map((record, index) => (
+                    <div className="ppe-container-element" key={index}>
+                        <div className="ppe-line-1">
+                            <div className="ppe-line-1-container">
+                                <div className="ppe-line-1-info">
+                                    <p className="ppe-line-1-degree">
+                                        {record.employer} ,{" "}
+                                        <p className="ppe-italic">
+                                            {record.jobTitle}
+                                        </p>
+                                    </p>
+                                </div>
+                                <div className="personal-buttons-container">
+                                    <button
+                                        className="ppc-edit-button"
+                                        onClick={editProfessionalExperience}
+                                    >
+                                        <img src={icon_edit} alt="edit icon" />
+                                    </button>
+                                    <button
+                                        className="ppc-delete-button"
+                                        onClick={deleteProfessionalExperience}
+                                    >
+                                        x
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="ppc-line-2">{record.jobTitle}</div>
+                    </div>
+                ))}
+            </div>
+        );
+    } else {
+        return "";
+    }
 }
