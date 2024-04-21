1072c784409a3dffec4081273e59251269fb4d1e
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-04-20 19:09:43 -0500

    Add initial output (needs styling)

--- src/components/ProfessionalExperience/PastProfessionalExperienceRecord.jsx
+++ src/components/ProfessionalExperience/PastProfessionalExperienceRecord.jsx
@@ -1,3 +1,23 @@
-export default function PastProfessionalExperienceRecord() {
-    return <div>hi</div>;
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+
+export default function PastProfessionalExperienceRecord({
+    record,
+    addClass = "",
+}) {
+    let classes = "education-output-section";
+
+    if (addClass && record.degree !== "") {
+        classes += " stored-education-output-active";
+    }
+
+    return (
+        <div className={classes}>
+            <span className="education-output-left">
+                {/* remove comma if additional information is not present */}
+                {record.employer}
+            </span>
+            <span className="education-output-right">{record.description}</span>
+        </div>
+    );
 }
